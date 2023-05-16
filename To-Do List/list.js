2a3d51204c13552b34e5cb47771950037b83106f
Sugiharto <sugihartocg@gmail.com>
2023-05-16 22:06:27 +0700

    mengerjakan to-do list versi saya

--- To-Do List/list.js
+++ To-Do List/list.js
@@ -1,17 +1,29 @@
-
-
 const tambah = document.querySelector('.tombol');
+const task = document.querySelector('.task');
 
 tambah.addEventListener('click', function(){
-    const task = document.querySelector('.task');
-    const tambahTask = document.createElement('input');
-    tambahTask.type = 'checkbox';
+    //first wrap
+    const tambahWrapTask = document.createElement('div');
+    tambahWrapTask.className = 'wrapTask';
 
-    const text = document.querySelector('.task')
-    const tambahText = document.createElement('input');
-    tambahText.type = 'text';
+    //its element
+    const tambahCheck = document.createElement('div');
+    tambahCheck.className = 'check';
+    const tambahFill = document.createElement('div');
+    tambahFill.className = 'fill';
 
-    task.appendChild(tambahTask);
-    text.appendChild(tambahText);
-    // text.style.display = "none";
+    //lets put some species
+    const tambahCheckInput = document.createElement('input');
+    tambahCheckInput.type = 'checkbox';
+    const tambahFillInput = document.createElement('input');
+    tambahFillInput.type = 'text';
+
+    //concanate it
+    tambahCheck.appendChild(tambahCheckInput);
+    tambahFill.appendChild(tambahFillInput);
+
+    tambahWrapTask.appendChild(tambahCheck);
+    tambahWrapTask.appendChild(tambahFill);
+
+    task.appendChild(tambahWrapTask);
 });
