b6bc64db51c1b2a35d2deaa9eb46b8701b196f94
Sugiharto <sugihartocg@gmail.com>
2023-06-16 20:25:50 +0700

    menyelesaikan desain CSS pada input textarea

--- To-Do List/t-app.js
+++ To-Do List/t-app.js
@@ -1,14 +1,4 @@
-//mengambil object yang dibutuhkan
-const addTask = document.querySelector('.button-icon');
-const taskContainer = document.querySelector('.task-container');
 
-addTask.addEventListener('click', function(){
-    const task = document.createElement('div');
-    task.className = 'task';
-
-    //masukkan ke container
-    taskContainer.appendChild(task);
-});
 
 
 
